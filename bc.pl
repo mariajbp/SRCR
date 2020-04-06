695591bd26a8d9f1174fb0a769d618a3423710a2
mariajbp <mariajbp00@gmail.com>
2020-04-06 16:33:42 +0100

    git push

--- bc.pl
+++ bc.pl
@@ -1,7 +1,7 @@
 %Extensão do predicato Contrato: IdAd, IdAda, TipoDeContrato, TipoDeProcedimento, Descrição, Valor, Prazo, Local, Data -> {V,F,D}
 
 % ------- Conhecimento Perfeito Positivo
-contrato(600018709,502381973, 'Aquisição de serviços', 'Ajuste Direto', 'Aquisição de serviços de acesso à DATAJURIS – Base de Dados Jurídicos', 4000, '1095 dias','Lisboa', '19-03-2020').
+contrato(600018709,502381973, 'Aquisição de serviços', 'Ajuste Direto', 'Aquisição de serviços de acesso à DATAJURIS – Base de Dados Jurídicos', 4000, '195 dias','Lisboa', '19-03-2020').
 contrato(506696464,809589087, 'Aquisição de serviços', 'Ajuste Direto','Reparação e Conservação de Escolas - Fornecimento de aluminos', 2800,'30 dias', 'Vila Flor', '16-02-2020').
 contrato(506901173,515204463, 'Aquisição de serviços', 'Ajuste Direto', 'Obras de Demolição e Reposição da Legalidade De Anexo', 4694.60,'25 dias','Braga','11-03-2020').
 contrato(502011378,980474710, 'Aquisição de bens móveis', 'Concurso público', 'Aquisição de um microscópio 2Photon', 320000, '105 dias', 'Braga', '31-03-2020').
@@ -13,6 +13,8 @@ contrato(508481287,508592909, 'Aquisição de bens móveis', 'Consulta Prévia',
 
 % ------- Conhecimento Perfeito Negativo
 
+-contrato(501128840,502605731,'Aquisição de serviços', 'Ajuste Direto', 'Prestação de Serviços de Informática para a Gestão dos Clientes', 4800.50, '365 dias', '17-03-2020').
+
 % ------- Conhecimento Imperfeito Incerto
 contrato(unknownidAd, 505044897, 'Aquisição de bens móveis', 'Ajuste Direto', 'Aquisição de materiais para a manutenção das infraestruturas da AdDP', 267.50, '3 dias', '02-04-2020').
 excecao(contrato(IdAd,IdAda,TContrato,TProcedimento,Descricao,Val,Prazo,Local,Data)) :- contrato(unknownidAd,IdAda,TContrato,TProcedimento,Descricao,Val,Prazo,Local,Data).
@@ -21,10 +23,12 @@ contrato(600014690, unknownidAda, 'Aquisição de serviços', 'Ajuste Direto', '
 excecao(contrato(IdAd,IdAda,TContrato,TProcedimento,Descricao,Val,Prazo,Local,Data)) :- contrato(IdAd,unknownidAda,TContrato,TProcedimento,Descricao,Val,Prazo,Local,Data).
 
 % ------- Conhecimento Imperfeito Impreciso
-contrato(506346773, 503504564, 'Locação de bens móveis', 'Contrato Público', 'Fornecimento de energia elétrica para as instalações da GESAMB', 1780.22, '365 dias','Évora' '18-01-2020'). 
-contrato(506346773, 980245974, 'Locação de bens móveis', 'Contrato Público', 'Fornecimento de energia elétrica para as instalações da GESAMB', 1780.22, '365 dias','Évora' '18-01-2020'). 
+excecao(contrato(506346773, 503504564, 'Locação de bens móveis', 'Contrato Público', 'Fornecimento de energia elétrica para as instalações da GESAMB', 1780.22, '365 dias','Évora' '18-01-2020')). 
+excecao(contrato(506346773, 980245974, 'Locação de bens móveis', 'Contrato Público', 'Fornecimento de energia elétrica para as instalações da GESAMB', 1780.22, '365 dias','Évora' '18-01-2020')).
+
 
 % ------- Conhecimento Imperfeito Interdito
+
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 %Extensão do predicato adjudicante: IdAd, Nome, NIF, Morada -> {V,F,D}
 
@@ -41,15 +45,17 @@ adjudicante(9, 'Centro Hospitalar Universitário de Lisboa Norte, E. P. E.', 508
 
 adjudicante(10,'Gesamb - Gestão Ambiental e de Resíduos',506346773, 'Évora').
 
-adjudicante(11, 'Secretaria-Geral da Presidência do Conselho de Ministros',600014690)
+adjudicante(11, 'Secretaria-Geral da Presidência do Conselho de Ministros',600014690).
 
 % ------- Conhecimento Perfeito Negativo
+-adjudicante(13, 'Município de Bragança', 501128840, 'Bragança').
 
 % ------- Conhecimento Imperfeito Incerto
-adjudicante(11, 'Águas do Douro e Paiva, S. A.', unknown_nif, 'Vila Nova de Gaia').
+adjudicante(12, 'Águas do Douro e Paiva, S. A.', unknown_nif, 'Vila Nova de Gaia').
 excecao(adjudicante(Id, Nome, Nif, Morada)) :- adjudicante(Id, Nome, unknown_nif, Morada).
 
 % ------- Conhecimento Imperfeito Impreciso
+
 
 % ------- Conhecimento Imperfeito Interdito
 
@@ -57,7 +63,7 @@ excecao(adjudicante(Id, Nome, Nif, Morada)) :- adjudicante(Id, Nome, unknown_nif
 %Extensão do predicato adjudicatária: IdAda, Nome, NIF, Morada -> {V,F,D}
 
 % ------- Conhecimento Perfeito Positivo
-adjudicataria(1, 'Data Juris - Direito e Informática Lda.', 502381973, 'Portugal')
+adjudicataria(1, 'Data Juris - Direito e Informática Lda.', 502381973, 'Portugal').
 adjudicataria(2, 'Carlos Manuel Pires', 809589087, 'Vila Flor').
 adjudicataria(3, 'Plateia de Cálculos Unipessoal, Lda', 515204463, 'Braga').
 adjudicataria(4, 'OLYMPUS IBERIA SAU', 980474710, 'Portugal').
@@ -66,15 +72,17 @@ adjudicataria(6, 'Pamafe Informática, Lda', 504099388, 'Portugal').
 adjudicataria(7, 'Accenture Tecnology Solutions - Soluções Informáticas Integradas, S.A.', 502443855, Portugal).
 adjudicataria(8, 'Beltrão Coelho, Lda.', 504654748, 'Portugal').
 adjudicataria(9, 'Batist Medical Portugal Unipessoal, Lda', 508592909, 'Portugal').
+adjudicataria(10, 'Endesa Energia, S.A. - Sucursal Portugal', 980245974, 'Portugal').
+adjudicataria(11, 'EDP', 503504564, 'Portugal').
 
 % ------- Conhecimento Perfeito Negativo
+-adjudicataria(13, 'CGITI Portugal,S.A', 502605731 'Portugal').
 
 % ------- Conhecimento Imperfeito Incerto
 adjudicataria(12, 'BIBLIOSOFT- Informática, Formação e Serviços, Lda.', unknown_nif, 'Portugal').
-excecao(adjudicataria(Id,Nome,Nif,Morada)) :- adjudicataria(Id,Nome,unknown_nif,Morada)-
+excecao(adjudicataria(Id,Nome,Nif,Morada)) :- adjudicataria(Id,Nome,unknown_nif,Morada).
 
 % ------- Conhecimento Imperfeito Impreciso
-adjudicataria(10, 'Endesa Energia, S.A. - Sucursal Portugal', 980245974, 'Portugal').
-adjudicataria(11, 'EDP', 503504564, 'Portugal').
+
 
 % ------- Conhecimento Imperfeito Interdito
--- invariantes.pl
+++ invariantes.pl
@@ -1,8 +1,8 @@
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Invariantes Estruturais e Referenciais
-%- - - - - - - - - - - - - - - - - - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Invariante que garante que não existe conhecimento perfeito positivo repetido
-+CPP :: (solucoes(CPP, CPP, S), comprimento(S, 1)).
++CPP :: (solucoes(CPP, CPP, S), comprimento(R, 1)).
 
 % Invariante que garante que não existe conhecimento perfeito negativo repetido
 
@@ -11,13 +11,39 @@
 % Invariante que não permite adicionar conhecimento perfeito negativo que contradiz conhecimento perfeito positivo
 
 % Invariante que garante que não existem excecoes repetidas
-+(excecao(E)) :: (solucoes(E, excecao(E), S), comprimento(S, 1)).
++(excecao(E)) :: (solucoes(E, excecao(E), R), comprimento(R, 1)).
 
-%- - - - - - - - - - - - - - - - - - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Invariantes Estruturais e Referenciais: Contrato
 
-%- - - - - - - - - - - - - - - - - - - - - - - - - - -  -  -  -  -   -
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Invariantes Estruturais e Referenciais: Adjudicante
 
-%- - - - - - - - - - - - - - - - - - - - - - - - - - -  -  -  -  -   -
+%Garantir que o id e nif de cada Adjudicante é único
++adjudicante(IdAd,Nome,Nif,Morada) :: (solucoes(IdAd, adjudicante(IdAd,_,_,_),R), comprimento(R,1)
+									  (solucoes(Nif, adjudicataria(_,Nif,_,_),R2), comprimento(R2,1))).
+
+% Garantir que adjudicantes com ids diferentes têm diferente informação
+% para conhecimento perfeito positivo
++adjudicante(IdAd,Nome,Nif,Morada) :: (solucoes((Nome, Nif,Morada), adjudicante(_,Nome,Nif,Morada), R), 
+									   comprimento(R,1)).
+
+% Garantir que não é possível remover uma entidade adjudicataria com um contrato
+-adjudicante(_,_,Nif,_) :: (solucoes(Nif), contrato(Nif,_,_,_,_,_,_,_,_), R),
+                            comprimento(R, 0)).
+						
+%--------------------------------- - - - - - - - - - -  -  -  -  -   -
 % Invariantes Estruturais e Referenciais: Adjudicataria
+
+%Garantir que o id e nif de cada entidade adjudicataria é único
++adjudicataria(IdAda, Nome, Nif, Morada) :: (solucoes(IdAda,adjudicataria(IdAda,_,_,_),R),comprimento(R,1),
+											(solucoes(Nif,adjudicataria(_,Nif,_,_),R2),comprimento(R2,1))).
+
+% Garantir que entidades adjudicatarias com ids diferentes têm diferente informação
+% para conhecimento perfeito positivo
++adjudicataria(IdAd,Nome,Nif,Morada) :: (solucoes((Nome, Nif,Morada), adjudicataria(_,Nome,Nif,Morada), R), 
+									     comprimento(R,1)).
+
+% Garantir que não é possível remover uma entidade adjudicataria com um contrato
+-adjudicataria(_,_,Nif,_) :: (solucoes(Nif), contrato(_,Nif,_,_,_,_,_,_,_), R),
+                              comprimento(R, 0)).

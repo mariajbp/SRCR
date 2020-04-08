f95c60d962410115fe02153fc787d42004de5d4a
mariajbp <mariajbp00@gmail.com>
2020-04-08 15:40:57 +0100

    FUCK

--- bc.pl
+++ bc.pl
@@ -1,38 +1,38 @@
-%Extensao do predicato Contrato: IdAd, IdAda, TipoDeContrato, TipoDeProcedimento, Descrição, Valor, Prazo, Local, Data -> {V,F,D}
+%Extensao do predicato Contrato: Id,IdAd, IdAda, TipoDeContrato, TipoDeProcedimento, Descrição, Valor, Prazo, Local, Data -> {V,F,D}
 % ------- Conhecimento Perfeito Positivo
-contrato(600018709,502381973, "Aquisicao de servicos", "Ajuste Direto", "Aquisicao de servicos de acesso da Base de Dados Juridicos", 4000, "195 dias", "Lisboa", "19-03-2020").
-contrato(506696464,809589087, "Aquisicao de serviços", "Ajuste Direto", "Reparacao e Conservacao de Escolas - Fornecimento de aluminos", 2800, "30 dias", "Vila Flor", "16-02-2020").
-contrato(506901173,515204463, "Aquisicao de servicos", "Ajuste Direto", "Obras de Demolicao e Reposicao da Legalidade De Anexo", 4694.60, "25 dias", "Braga", "11-03-2020").
-contrato(502011378,980474710, "Aquisicao de bens moveis", "Concurso publico", "Aquisicao de um microscopio 2Photon", 320000, "105 dias", "Braga", "31-03-2020").
-contrato(680047360,500853975, "Aquisicao de bens moveis", "Concurso publico", "Aquisicao de cafe por lotes para os anos de 2020 e 2021", 157275, "353 dias", "Braga", "13-01-2020").
-contrato(501413197,504099388, "Locacao de bens moveis", "Concurso publico", "Aquisicao de nos de computacao (servidores) para cloud interna", 210000, "30 dias", "Porto", "16-03-2020").
-contrato(600084779,502443855, "Aquisicao de servicos", "Consulta Previa", "Aquisicao de servicos de desenvolvimento aplicacional dos sistemas de informacao da Autoridade Tributaria e Aduaneira", 201400, "210 dias", "19-02-2020").
-contrato(600006638,504654748, "Aquisicao de bens moveis", "Consulta Previa", "Aquisicao de 2 equipamentos multifuncionais", 10980, "30 dias", "12-03-2020").
+contrato(1,600018709,502381973, "Aquisicao de servicos", "Ajuste Direto", "Aquisicao de servicos de acesso da Base de Dados Juridicos", 4000, "195 dias", "Lisboa", "19-03-2020").
+contrato(2,506696464,809589087, "Aquisicao de serviços", "Ajuste Direto", "Reparacao e Conservacao de Escolas - Fornecimento de aluminos", 2800, "30 dias", "Vila Flor", "16-02-2020").
+contrato(3,506901173,515204463, "Aquisicao de servicos", "Ajuste Direto", "Obras de Demolicao e Reposicao da Legalidade De Anexo", 4694.60, "25 dias", "Braga", "11-03-2020").
+contrato(4,502011378,980474710, "Aquisicao de bens moveis", "Concurso publico", "Aquisicao de um microscopio 2Photon", 320000, "105 dias", "Braga", "31-03-2020").
+contrato(5,680047360,500853975, "Aquisicao de bens moveis", "Concurso publico", "Aquisicao de cafe por lotes para os anos de 2020 e 2021", 157275, "353 dias", "Braga", "13-01-2020").
+contrato(6,501413197,504099388, "Locacao de bens moveis", "Concurso publico", "Aquisicao de nos de computacao (servidores) para cloud interna", 210000, "30 dias", "Porto", "16-03-2020").
+contrato(7,600084779,502443855, "Aquisicao de servicos", "Consulta Previa", "Aquisicao de servicos de desenvolvimento aplicacional dos sistemas de informacao da Autoridade Tributaria e Aduaneira", 201400, "210 dias", "19-02-2020").
+contrato(8,600006638,504654748, "Aquisicao de bens moveis", "Consulta Previa", "Aquisicao de 2 equipamentos multifuncionais", 10980, "30 dias", "12-03-2020").
 
 
 % ------- Conhecimento Perfeito Negativo
--contrato(501128840,502605731, "Aquisicao de servicos", "Ajuste Direto", "Prestacao de Servicos de Informatica para a Gestao dos Clientes", 4800.50, "365 dias", "17-03-2020").
+-contrato(9,501128840,502605731, "Aquisicao de servicos", "Ajuste Direto", "Prestacao de Servicos de Informatica para a Gestao dos Clientes", 4800.50, "365 dias", "17-03-2020").
 
 
 % ------- Conhecimento Imperfeito Incerto
 %Não se sabe o prazo do contrato
-contrato(506346773, 503504564, "Locacao de bens moveis", "Concurso publico", "Fornecimento de energia eletrica em BTN e MT para as instalacoes da GESAMB", 1780.22, prazo_desconhecido, "18-01-2019").
-excecao(contrato(IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data)) :- contrato(IdA,IdAda,Tc,TP,Desc,Val,prazo_desconhecido,Local,Data).
+contrato(10,506346773, 503504564, "Locacao de bens moveis", "Concurso publico", "Fornecimento de energia eletrica em BTN e MT para as instalacoes da GESAMB", 1780.22, prazo_desconhecido, "18-01-2019").
+excecao(contrato(Id,IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data)) :- contrato(Id,IdA,IdAda,Tc,TP,Desc,Val,prazo_desconhecido,Local,Data).
 
 % ------- Conhecimento Imperfeito Impreciso
 %Não se sabe se o valor do contrato é 500€ ou 4700€
-excecao(contrato(600086992, 503188620, "Locacao de bens moveis", "Concurso Publico", "Contratacao de 2 veiculos eletricos em regime de aluguer operacional de veiculos", 5000, "140 dias", "Portugal", "16-01-2020")).
-excecao(contrato(600086992, 503188620, "Locacao de bens moveis", "Concurso Publico", "Contratacao de 2 veiculos eletricos em regime de aluguer operacional de veiculos", 4700, "140 dias", "Portugal", "16-01-2020")).
+excecao(contrato(11,600086992, 503188620, "Locacao de bens moveis", "Concurso Publico", "Contratacao de 2 veiculos eletricos em regime de aluguer operacional de veiculos", 5000, "140 dias", "Portugal", "16-01-2020")).
+excecao(contrato(11,600086992, 503188620, "Locacao de bens moveis", "Concurso Publico", "Contratacao de 2 veiculos eletricos em regime de aluguer operacional de veiculos", 4700, "140 dias", "Portugal", "16-01-2020")).
 
 % ------- Conhecimento Imperfeito Interdito
 
 %É impossivel saber o valor do contrato
-contrato(508481287,508592909, "Aquisicao de bens moveis", "consulta Previa", "Aquisicao de seringas", valor_desconhecido, "352 dias", "14-01-2020").
-excecao(contrato(IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data)) :- contrato(contrato(IdA,IdAda,Tc,TP,Desc,valor_desconhecido,Pr,Local,Data)).
+contrato(12,508481287,508592909, "Aquisicao de bens moveis", "consulta Previa", "Aquisicao de seringas", valor_desconhecido, "352 dias", "14-01-2020").
+excecao(contrato(Id,IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data)) :- contrato(contrato(Id,IdA,IdAda,Tc,TP,Desc,valor_desconhecido,Pr,Local,Data)).
 nulointerdito(valor_desconhecido).
 
-+contrato(IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data) :: (solucoes((IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data),
-                                                      contrato(508481287,508592909, "Aquisicao de bens moveis", "Consulta Previa", "Aquisicao de seringas", valor_desconhecido, "352 dias", "14-01-2020"),
++contrato(Id,IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data) :: (solucoes((Id,IdA,IdAda,Tc,TP,Desc,Val,Pr,Local,Data),
+                                                      contrato(12,508481287,508592909, "Aquisicao de bens moveis", "Consulta Previa", "Aquisicao de seringas", valor_desconhecido, "352 dias", "14-01-2020"),
                                                       nao(nulointerdito(valor_desconhecido)),R, comprimento(R,0))).
 
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
@@ -114,6 +114,6 @@ nulointerdito(unknown_address).
                                        adjudicataria(5,"Manuel Rui Azinhais Nabeiro Lda", 500853975, unknown_address),
                                        nao(nulointerdito(unknown_address)), R), comprimento(R,0)).
 
-%-------------------- Predicados de Id
+% -------  Predicados de Id
 idAdjud(12).
 idAdjudicata(13).
